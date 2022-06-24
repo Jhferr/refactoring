306bd63ac1d560b841b56e766ac2cdbd0bae8ca1
Jorge Henrique Ferreira da Silva <68034513+Jhferr@users.noreply.github.com>
2022-06-24 11:38:42 -0300

    Template Method - Parte 1
    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,34 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+   public String getStartOfResult (Customer aCustomer){
+      return "Rental Record for " + aCustomer.getName() +
+      "\n";
+   }
+   
+   public String getMiddleOfResult (Customer each){
+      return "\t" + each.getMovie().getTitle()+ "\t" +
+         String.valueOf(each.getCharge()) + "\n";
+   }
+
+   public String getEndOfResult (Customer aCustomer){
+      return "Amount owed is " +
+      String.valueOf(aCustomer.getTotalCharge()) + "\n" +
+      "You earned " +
+      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+      " frequent renter points";
+   }
+
    public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
+      String result = getStartOfResult(aCustomer);
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
          //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
+         result += getMiddleOfResult(each);
       }
       //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
+      result += getEndOfResult(aCustomer);
       return result;
    }
 }
